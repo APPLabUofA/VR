040f076f15e7cd918a3ed16cdf1b127e2e056d3d
Eden Redman <46800990+Existentialist-Robot@users.noreply.github.com>
2019-03-18 13:45:53 -0600

    Update master.m

--- Matlab_Scripts/5_Depth_Colourless/Pilot/master.m
+++ Matlab_Scripts/5_Depth_Colourless/Pilot/master.m
@@ -1,16 +1,16 @@
-% You run and change parameters for preprocessing and analysis here, you
-% also specify the target condition & event type
-Wrapper_VR_P3_Depth_Colourless_Pilot
+% You run and change parameters for preprocessing and analysis with this one script, from this script you can also change the target condition & event type
+% Running this script will create both **exp** & **anal** structures which are used in preprocessing & analysis
+1. Wrapper_VR_P3_Depth_Colourless_Pilot
 
-% This script renames triggers appropriately, refer to markerinfo.txt
+% This script renames triggers appropriately, *refer to markerinfo.txt*
 % and initializes structures in accordance with wrapper settings above
-Preprocessing_VR_P3_Depth_Colourless(exp)
+2. Preprocessing_VR_P3_Depth_Colourless(**exp**)
 
-% Loads data for use with ERP (we are not using time-frequency analysis)
-Analysis_VR_P3_Depth_Colourless(exp,anal)
+% Loads data for use with ERP (note: we are not using time-frequency analysis)
+3. Analysis_VR_P3_Depth_Colourless(**exp,anal**)
 
-%This script plots the ERPs & topographies
-VR_P3_Depth_Colourless_ERP
+%This script plots the ERP, topographies, and bar graphs
+4. VR_P3_Depth_Colourless_ERP
 
 
 
